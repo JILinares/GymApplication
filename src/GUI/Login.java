8c4cdea0e0a2903c216e1d1a58edc28ca811d1d9
QUINTIX <quintix128@gmail.com>
2016-04-14 13:07:13 -0400

    JDialog Login
    Currently nonfunctional dialog; would rather not have another Jdialog
    called within the Login button event handler, rather go about program
    logic (outside of checking if the login is authorized) in Main()

---
+++ src/GUI/Login.java
@@ -0,0 +1,130 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.AuthLogin;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtUserName;
+	private JPasswordField passwordField;
+	private AuthLogin[] logins;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Login dialog = new Login();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public AuthLogin returnedLogin;
+	
+	/**
+	 * Create the dialog.
+	 */
+	public Login() {
+		logins = AuthLogin.readFile();
+		
+		setTitle("Gym");
+		setBounds(100, 100, 419, 179);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{87, 255, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblNewLabel = new JLabel("Staff Member Login");
+			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+			gbc_lblNewLabel.anchor = GridBagConstraints.NORTH;
+			gbc_lblNewLabel.gridwidth = 2;
+			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel.gridx = 0;
+			gbc_lblNewLabel.gridy = 0;
+			contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+		}
+		{
+			JLabel lblUsername = new JLabel("Username:");
+			GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+			gbc_lblUsername.anchor = GridBagConstraints.EAST;
+			gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+			gbc_lblUsername.gridx = 0;
+			gbc_lblUsername.gridy = 1;
+			contentPanel.add(lblUsername, gbc_lblUsername);
+		}
+		{
+			txtUserName = new JTextField();
+			GridBagConstraints gbc_txtUserName = new GridBagConstraints();
+			gbc_txtUserName.insets = new Insets(0, 0, 5, 5);
+			gbc_txtUserName.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtUserName.gridx = 1;
+			gbc_txtUserName.gridy = 1;
+			contentPanel.add(txtUserName, gbc_txtUserName);
+			txtUserName.setColumns(10);
+		}
+		{
+			JLabel lblPassword = new JLabel("Password:");
+			lblPassword.setVerticalAlignment(SwingConstants.BOTTOM);
+			GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+			gbc_lblPassword.anchor = GridBagConstraints.EAST;
+			gbc_lblPassword.insets = new Insets(0, 0, 0, 5);
+			gbc_lblPassword.gridx = 0;
+			gbc_lblPassword.gridy = 2;
+			contentPanel.add(lblPassword, gbc_lblPassword);
+		}
+		{
+			passwordField = new JPasswordField();
+			GridBagConstraints gbc_passwordField = new GridBagConstraints();
+			gbc_passwordField.insets = new Insets(0, 0, 0, 5);
+			gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_passwordField.gridx = 1;
+			gbc_passwordField.gridy = 2;
+			contentPanel.add(passwordField, gbc_passwordField);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Login");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						//Todo: test and close
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+
+}
